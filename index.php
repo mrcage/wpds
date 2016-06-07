8e7bf6796d05bdf33b81d3711f064b80bd1e987a
Nicolas Perrenoud <mail@nicu.ch>
2016-06-07 11:19:39 +0200

    Code cleanup

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ get_header(); ?>
 					$options[] = $k . ':'. $val;
 				}
 			}
-			echo '<ul data-orbit' . ( !empty($options) ? ' data-options="' . implode(';', $options) . '"' : '') . '>';
+			echo '<ul data-orbit' . ( !empty($options) ? ' data-options="' . implode(';', $options) . '"' : '') . '>' . "\n";
 
 	            $args=array(
 	                'post_type' => 'post',
@@ -37,22 +37,22 @@ get_header(); ?>
 		$head_color = get_color_option($post->ID, 'headline-color');
 		$subhead_color = get_color_option($post->ID, 'subhead-color');
 		$copy_color = get_color_option($post->ID, 'copy-color');
-              $link = get_post_meta($post->ID, 'link', true);
+		$link = get_post_meta($post->ID, 'link', true);
 
 		if($background_image != '') :
-                	echo '<li class="post-box large-12 columns" style="background:url(' . $background_image . ') 0 0 no-repeat; width:100%; height:100%; background-size:contain;"></li>';
+                	echo '<li class="post-box large-12 columns" style="background:url(' . $background_image . ') 0 0 no-repeat; width:100%; height:100%; background-size:contain;"></li>' . "\n";
 		else :
 			echo '<li class="post-box large-12 columns"' . ( !empty($background_color) ? ' style="background:#' . $background_color . ';"' : '' ) . '>',
-          						'<h1' . ( !empty($head_color) ? ' style="color:#' . $head_color . ';"' : '' ) . '>' . get_the_title() . '</h1>',
-          						'<h2' . ( !empty($subhead_color) ? ' style="color:#' . $subhead_color . ';"' : '' ) . '>' . get_post_meta($post->ID, 'subtitle', true) . '</h2>',
+          						'<h1' . ( !empty($head_color) ? ' style="color:#' . $head_color . ';"' : '' ) . '>' . get_the_title() . '</h1>' . "\n",
+          						'<h2' . ( !empty($subhead_color) ? ' style="color:#' . $subhead_color . ';"' : '' ) . '>' . get_post_meta($post->ID, 'subtitle', true) . '</h2>' . "\n",
           						'<div class="row">',
-          						'<a href="' . get_post_meta($post->ID, 'link', true) . '">',
+          						!empty($link) ? '<a href="' . get_post_meta($post->ID, 'link', true) . '">' : '',
           						get_the_post_thumbnail($post_id, 'large', array('class' => 'large-3 columns feature')),
-          						'</a>',
+          						!empty($link) ? '</a>' : '',
           						'<p class="large-7 columns copy end"' . ( !empty($copy_color) ? ' style="color:#' . $copy_color . ';"' : '' ) . '>' . do_shortcode( get_the_content() ) . '</p>',
-          						'<p class="link"><a' . ( !empty($copy_color) ? ' style="color:#' . $copy_color . ';"' : '' ) . ' href="' . get_post_meta($post->ID, 'link', true) . '">' . get_post_meta($post->ID, 'link', true) . '</a>',
+          						!empty($link) ? '<p class="link"><a' . ( !empty($copy_color) ? ' style="color:#' . $copy_color . ';"' : '' ) . ' href="' . $link . '">' . $link . '</a>' : '',
           						'</div>',
-          						'</li>';
+          						'</li>' ."\n";
               endif;
 	            endwhile;
 	            endif;
