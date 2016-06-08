2901c724594cf1d9a5bad4c83b3fcdc2f01133a9
Nicolas Perrenoud <mail@nicu.ch>
2016-06-08 13:57:12 +0200

    Do a connection test before reloading the page

--- javascripts/vendor/app.js
+++ javascripts/vendor/app.js
@@ -1,7 +1,23 @@
 jQuery(document).ready(function ($) {
 	var reloadTimeout = typeof defaultReloadTimeout !== 'undefined' ? defaultReloadTimeout : 5 * 60 * 1000;
-/* REFRESH PAGE EVERY FIVE MINUTES ----*/
-window.setTimeout('location.reload(true)', reloadTimeout);
-/* /REFRESH PAGE EVERY FIVE MINUTES ---- */
-
+	doReloadPage(reloadTimeout);
 });
+
+function doReloadPage(reloadTimeout) {
+	if (reloadTimeout > 0) {
+		window.setTimeout(function() {
+			console.log('Trying to reload...');
+			$.ajax({
+				url  : "/",
+				type : "HEAD"
+			})
+			.done(function() {
+				location.reload();
+			})
+			.fail(function( jqXHR, textStatus, errorThrown ) {
+				console.log('Unable to reload: ' + textStatus);
+				window.setTimeout(function(){ doReloadPage(reloadTimeout) }, 5000);
+			});
+		}, reloadTimeout);
+	}
+}
