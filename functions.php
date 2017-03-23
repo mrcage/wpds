ddd0ab86ac1516450110da1a1303ace9aef2a0b5
Nicolas Perrenoud <mail@nicu.ch>
2017-03-23 07:52:53 +0100

    Fixed show post today update

--- functions.php
+++ functions.php
@@ -878,7 +878,9 @@ function get_post_status_hash() {
 	);
 	$the_query = new WP_Query($args);
 	if ($the_query->have_posts()) : while ( $the_query->have_posts() ) : $the_query->the_post();
-		$data[] = $post->ID.":".$post->post_modified;
+		if (show_post_today( $post->ID )) {
+			$data[] = $post->ID.":".$post->post_modified;
+		}
 	endwhile; endif;
 	wp_reset_query();
 	return md5(implode(";", $data));
